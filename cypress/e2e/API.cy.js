f222d84afe46e001058538cdfbe894b824568aa1
techtmtHY84 <Timothyaldre@gmail.com>
2025-04-30 09:42:32 +0700

    add scenario cypress

--- cypress/e2e/API.cy.js
+++ cypress/e2e/API.cy.js
@@ -1,5 +1,19 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+  it('should get list of posts', () => {
+    cy.request('https://jsonplaceholder.typicode.com/posts').then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body).to.be.an('array');
+      expect(response.body.length).to.be.greaterThan(0);
+    });
+  });
+
+  it('should return 404 for invalid endpoint', () => {
+    cy.request({
+      url: 'https://jsonplaceholder.typicode.com/invalid-endpoint',
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(404);
+    });
+  });
+
 })
--- cypress/e2e/UI.cy.js
+++ cypress/e2e/UI.cy.js
@@ -1,5 +1,21 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  it('should login with correct credentials', () => {
+    cy.visit('https://the-internet.herokuapp.com/login');
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('button[type="submit"]').click();
+
+    cy.url().should('include', '/secure');
+    cy.get('.flash.success').should('contain', 'You logged into a secure area!');
   })
+  
+    it('should not login with incorrect credentials', () => {
+    cy.visit('https://the-internet.herokuapp.com/login');
+    cy.get('#username').type('wronguser');
+    cy.get('#password').type('wrongpass');
+    cy.get('button[type="submit"]').click();
+
+    cy.get('.flash.error').should('contain', 'Your username is invalid!');
+    cy.url().should('include', '/login');
+  });
 })
